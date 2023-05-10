7ba046e4ad1a9371a4ecbfe2c87be19090d88ada
Joshua Douce <Joshuadouce@gmail.com>
2023-05-10 21:28:55 +0100

    Add new basket state tests

--- Blackbird.RazorComponents.Tests/States/BasketStateTests.cs
+++ Blackbird.RazorComponents.Tests/States/BasketStateTests.cs
@@ -47,5 +47,56 @@ namespace Blackbird.RazorComponents.Tests.States
             _basketState.BasketItems.Should().ContainSingle();
             _basketState.BasketItemCount.Should().Be(expectedQuantity);
         }
+
+        [Fact]
+        public void RemoveItemFromBasket_ShouldRemoveEntireItem()
+        {
+            // Arrange
+            var basketState = new BasketState();
+            var product = new ProductDto { ProductId = "1", Name = "Test Product", Price = 10, Quantity = 1 };
+            basketState.AddToBasket(product);
+
+            // Act
+            basketState.RemoveItemFromBasket(product);
+
+            // Assert
+            Assert.Empty(basketState.BasketItems);
+        }
+
+        [Fact]
+        public void DecreaseQuantity_ShouldDecreaseItemQuantityByOne()
+        {
+            // Arrange
+            var basketState = new BasketState();
+            var product = new ProductDto { ProductId = "1", Name = "Test Product", Price = 10, Quantity = 2 };
+            basketState.AddToBasket(product);
+
+            // Act
+            basketState.RemoveItemFromBasket(product);
+
+            // Assert
+            Assert.Single(basketState.BasketItems);
+            Assert.Equal(1, basketState.BasketItems[0].Quantity);
+        }
+
+        [Fact]
+        public void ClearItemFromBasket_ShouldClearItems()
+        {
+            // Arrange
+            var basketState = new BasketState();
+            var product1 = new ProductDto { ProductId = "1", Name = "Test Product 1", Price = 10, Quantity = 1 };
+            var product2 = new ProductDto { ProductId = "2", Name = "Test Product 2", Price = 20, Quantity = 1 };
+            basketState.AddToBasket(product1);
+            basketState.AddToBasket(product2);
+
+            // Act
+            basketState.ClearItemFromBasket(product1);
+
+            // Assert
+            Assert.Single(basketState.BasketItems);
+            Assert.DoesNotContain(product1, basketState.BasketItems);
+            Assert.Contains(product2, basketState.BasketItems);
+        }
+
     }
 }
